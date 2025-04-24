8f0e0153f65865a3a921a62bc92a95a57729f587
lapulga-10-messi <avamsi2k11@gmail.com>
2025-04-25 00:31:34 +0530

    Update streamlit_crop.py

--- streamlit_crop.py
+++ streamlit_crop.py
@@ -3,7 +3,7 @@ from mysql.connector import Error
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import streamlit as st
 
 # Database configuration
