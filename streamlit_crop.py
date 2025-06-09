ad602de72ab64f2f850392825c37269b6e17b54b
Vamsi vikas Amujala <avamsi2k11@gmail.com>
2025-06-09 18:49:09 +0530

    Update streamlit_crop.py

--- streamlit_crop.py
+++ streamlit_crop.py
@@ -15,6 +15,7 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 from scipy import stats 
+import joblib
 
 # Database configuration
 config = {
